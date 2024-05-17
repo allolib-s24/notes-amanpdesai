0b16fa1c234169690c2db80982b7bd26b796ca03
Aman Desai <100175330+amanpdesai@users.noreply.github.com>
2024-05-16 17:36:40 -0700

    Delete AMandSineSynth/build/AM/Release directory

--- AMandSineSynth/build/AM/Release/CMakeFiles/AM_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/AM_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/AM_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
